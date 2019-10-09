6b73cbe8cc6e2aebef441d91d81e71c829cae551
Angelique Nehmzow <angelique@nehmzow.com>
2019-10-09 15:29:15 -0700

    set react component state with passed-in listingId, invoke ajax call in componentDidMount with listingId to set state with photos (dummy data for now), server route to handle get req at /photos/:listingid path

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,4 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import $ from 'jquery';
 
-ReactDOM.render(<div>Test!</div>, document.getElementById('photo-gallery'));
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      listingId: props.listingId,
+      photos: []
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Listing ID: {this.state.listingId}</div>
+        <div>First photo URL: {(this.state.photos[0]) && this.state.photos[0].url}</div>
+        <div>First photo description: {(this.state.photos[0]) && this.state.photos[0].description}</div>
+      </div>
+    )
+  }
+
+  componentDidMount() {
+    $.get(`/photos/${this.state.listingId}`, null, (photos) => {
+      console.log('photos received for listing', photos);
+      this.setState({photos: photos});
+    }, 'json');
+  }
+
+}
+
+ReactDOM.render(<App listingId={0} />, document.getElementById('photo-gallery'));
--- server/index.js
+++ server/index.js
@@ -5,8 +5,19 @@ const port = 3001;
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  console.log('get req to / received!')
   res.end();
+});
+
+app.get('/photos/:listingid', (req, res) => {
+  var listingId = req.params.listingid;
+  var dummyData = {
+    listingId: listingId,
+    photos: [
+      {url: 'www.aws.amazon/someUrl0', description: 'some description 0'},
+      {url: 'www.aws.amazon/someUrl1', description: 'some description 1'},
+      {url: 'www.aws.amazon/someUrl2',description: 'some description 2'},
+    ]};
+  res.send(dummyData.photos);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));

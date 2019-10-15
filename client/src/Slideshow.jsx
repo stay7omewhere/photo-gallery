e10ffea0be2ebbd389a86bc78c463ebd7315c89a
Angelique Nehmzow <angelique@nehmzow.com>
2019-10-15 15:04:14 -0700

    add svg icons for slideshow buttons

--- client/src/Slideshow.jsx
+++ client/src/Slideshow.jsx
@@ -7,10 +7,22 @@ var Slideshow = function({photos, currentPhotoIndex, viewPhotoHandler, closeSlid
   var nextPhotoIndex = (currentPhotoIndex === photos.length - 1) ? 0 : currentPhotoIndex + 1;
   return (
     <div>
-      <button id="previous-photo" onClick={() => viewPhotoHandler(previousPhotoIndex)}>Previous</button>
+      <button id="previous-photo" onClick={() => viewPhotoHandler(previousPhotoIndex)}>
+        <svg viewBox="0 0 18 18" role="presentation" aria-label="Previous" aria-hidden="true" focusable="false" style={{height: '24px', width: '24px', fill: 'rgb(72, 72, 72)'}}>
+          <path d="m13.7 16.29a1 1 0 1 1 -1.42 1.41l-8-8a1 1 0 0 1 0-1.41l8-8a1 1 0 1 1 1.42 1.41l-7.29 7.29z" fillRule="evenodd"></path>
+        </svg>
+      </button>
       <img src={currentPhoto.url} height="350" onClick={() => viewPhotoHandler(nextPhotoIndex)}></img>
-      <button id="next-photo" onClick={() => viewPhotoHandler(nextPhotoIndex)}>Next</button>
-      <button id="close-slideshow" onClick={closeSlideshow}>Close Slideshow</button>
+      <button id="next-photo" onClick={() => viewPhotoHandler(nextPhotoIndex)}>
+        <svg viewBox="0 0 18 18" role="presentation" aria-label="Next" aria-hidden="true" focusable="false" style={{height: '24px', width: '24px', fill: 'rgb(72, 72, 72)'}}>
+          <path d="m4.29 1.71a1 1 0 1 1 1.42-1.41l8 8a1 1 0 0 1 0 1.41l-8 8a1 1 0 1 1 -1.42-1.41l7.29-7.29z" fillRule="evenodd"></path>
+        </svg>
+      </button>
+      <button id="close-slideshow" onClick={closeSlideshow}>
+        <svg viewBox="0 0 24 24" role="img" aria-label="Close" focusable="false" style={{height: '24px', width: '24px', display: 'block', fill: 'rgb(72, 72, 72)'}}>
+          <path d="m23.25 24c-.19 0-.38-.07-.53-.22l-10.72-10.72-10.72 10.72c-.29.29-.77.29-1.06 0s-.29-.77 0-1.06l10.72-10.72-10.72-10.72c-.29-.29-.29-.77 0-1.06s.77-.29 1.06 0l10.72 10.72 10.72-10.72c.29-.29.77-.29 1.06 0s .29.77 0 1.06l-10.72 10.72 10.72 10.72c.29.29.29.77 0 1.06-.15.15-.34.22-.53.22" fillRule="evenodd"></path>
+        </svg>
+      </button>
       <div id="photo-order">{currentPhotoIndex + 1} / {photos.length}</div>
       <div>{currentPhoto.description}</div>
     </div>

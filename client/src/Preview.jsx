6d41856f195d9a4a9ce43c338b1e11335f3be849
Angelique Nehmzow <angelique@nehmzow.com>
2019-10-16 20:18:44 -0700

    add borders/outlines, position view photos button

--- client/src/Preview.jsx
+++ client/src/Preview.jsx
@@ -5,21 +5,19 @@ var Preview = function({photos, viewPhotoHandler}) {
   var photoCount = photos.length;
   return (
     photoCount > 1 &&
-    <div className={styles.imgPanel}>
-      <div className={styles.imgPanelContainer}>
-        <div className={styles.primaryItem}>
-          <img className={styles.img} key={0} src={photos[0].url} onClick={() => viewPhotoHandler(0)}></img>
-        </div>
-        <div className={styles.secondaryItemsContainer}>
-          <div className={styles.secondaryItem}>
-            <img className={styles.img} key={1} src={photos[1].url} onClick={() => viewPhotoHandler(1)}></img>
-          </div>
-          <div className={styles.secondaryItem}>
-            <img className={styles.img} key={2} src={photos[2].url} onClick={() => viewPhotoHandler(2)}></img>
-          </div>
-        </div>
+    <div className={styles.layoutContainer}>
+      <div className={styles.primaryImg}>
+        <img className={styles.img} key={0} src={photos[0].url} onClick={() => viewPhotoHandler(0)}></img>
       </div>
-      <button className={styles.viewPhotos} onClick={() => viewPhotoHandler(0)}>View Photos</button>
+      <div className={styles.secondaryImgsContainer}>
+        <div className={styles.secondaryImg}>
+          <img className={styles.img} key={1} src={photos[1].url} onClick={() => viewPhotoHandler(1)}></img>
+        </div>
+        <div className={styles.secondaryImg}>
+          <img className={styles.img} key={2} src={photos[2].url} onClick={() => viewPhotoHandler(2)}></img>
+        </div>
+        <button className={styles.viewPhotos} onClick={() => viewPhotoHandler(0)}>View Photos</button>
+      </div>
     </div>
   );
 };

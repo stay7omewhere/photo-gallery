9467a99bf48357c6918d06ac9a596ab20b1246d1
Angelique Nehmzow <angelique@nehmzow.com>
2019-10-17 22:27:50 -0700

    add share and save icons

--- client/src/Preview.jsx
+++ client/src/Preview.jsx
@@ -2,14 +2,31 @@ import React from 'react';
 import styles from './preview.css';
 import cx from 'classnames';
 
+var arrowStemSvgPathDef = 'm11.5 16v-15';
+var arrowTipSvgPathDef = 'm7.5 4 4-3 4 3';
+var partialBoxSvgPathDef = 'm5.4 9.5h-3.9v14h20v-14h-3.1';
+var heartSvgPathDef = 'm17.5 2.9c-2.1 0-4.1 1.3-5.4 2.8-1.6-1.6-3.8-3.2-6.2-2.7-1.5.2-2.9 1.2-3.6 2.6-2.3 4.1 1 8.3 3.9 11.1 1.4 1.3 2.8 2.5 4.3 3.6.4.3 1.1.9 1.6.9s1.2-.6 1.6-.9c3.2-2.3 6.6-5.1 8.2-8.8 1.5-3.4 0-8.6-4.4-8.6';
+
 var Preview = function({photos, viewPhotoHandler}) {
   var photoCount = photos.length;
   return (
     photoCount > 1 &&
     <div className={styles.preview}>
       <div className={styles.listingActions}>
-        <button className={cx(styles.button, styles.shareListing)}>Share</button>
-        <button className={cx(styles.button, styles.saveListing)}>Save</button>
+        <button className={cx(styles.button, styles.shareListing)}>
+          <svg className={styles.arrowBox} viewBox="0 0 24 24">
+            <g fill-rule="evenodd">
+              <path d={arrowStemSvgPathDef}></path>
+              <path d={arrowTipSvgPathDef}></path>
+              <path d={partialBoxSvgPathDef}></path>
+            </g>
+          </svg>Share
+        </button>
+        <button className={cx(styles.button, styles.saveListing)}>
+          <svg className={styles.heart} viewBox="0 0 24 24">
+            <path d={heartSvgPathDef}></path>
+          </svg>Save
+        </button>
       </div>
       <button className={cx(styles.button, styles.viewPhotos)} onClick={() => viewPhotoHandler(0)}>View Photos</button>
       <div className={styles.photosContainer}>

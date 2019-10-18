3a0b21091b4267a5cb1684247a275ad558cd9a4e
Angelique Nehmzow <angelique@nehmzow.com>
2019-10-17 17:41:50 -0700

    put preview button styling into single class, install cs to use multiple classes for an element, add hover effects for images in grid

--- client/src/Preview.jsx
+++ client/src/Preview.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import styles from './preview.css';
+import cx from 'classnames';
 
 var Preview = function({photos, viewPhotoHandler}) {
   var photoCount = photos.length;
   return (
     photoCount > 1 &&
-    <div className={styles.layoutContainer}>
-      <div className={styles.primaryImg}>
-        <img className={styles.img} key={0} src={photos[0].url} onClick={() => viewPhotoHandler(0)}></img>
+    <div className={styles.preview}>
+      <div className={styles.listingActions}>
+        <button className={cx(styles.button, styles.shareListing)}>Share</button>
+        <button className={cx(styles.button, styles.saveListing)}>Save</button>
       </div>
-      <div className={styles.secondaryImgsContainer}>
-        <div className={styles.listingActions}>
-          <button className={styles.shareListing}>Share</button>
-          <button className={styles.saveListing}>Save</button>
+      <button className={cx(styles.button, styles.viewPhotos)} onClick={() => viewPhotoHandler(0)}>View Photos</button>
+      <div className={styles.photosContainer}>
+        <div className={styles.primaryImg}>
+          <img className={styles.img} key={0} src={photos[0].url} onClick={() => viewPhotoHandler(0)}></img>
         </div>
-        <div className={styles.secondaryImg}>
-          <img className={styles.img} key={1} src={photos[1].url} onClick={() => viewPhotoHandler(1)}></img>
+        <div className={styles.secondaryImgsContainer}>
+          <div className={styles.secondaryImg}>
+            <img className={styles.img} key={1} src={photos[1].url} onClick={() => viewPhotoHandler(1)}></img>
+          </div>
+          <div className={styles.secondaryImg}>
+            <img className={styles.img} key={2} src={photos[2].url} onClick={() => viewPhotoHandler(2)}></img>
+          </div>
         </div>
-        <div className={styles.secondaryImg}>
-          <img className={styles.img} key={2} src={photos[2].url} onClick={() => viewPhotoHandler(2)}></img>
-        </div>
-        <button className={styles.viewPhotos} onClick={() => viewPhotoHandler(0)}>View Photos</button>
       </div>
     </div>
   );

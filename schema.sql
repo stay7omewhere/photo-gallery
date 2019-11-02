f45e2cc634be435cb431a4b70e12f7fc8744182f
Faith <pfaithmtan@gmail.com>
2019-11-02 10:58:36 -0700

    Create a relation table between users and listings
    - Handle many-to-many relationship

--- schema.sql
+++ schema.sql
@@ -13,14 +13,22 @@ CREATE TABLE photos (
 
 CREATE TABLE listings (
   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  usersId INT NOT NULL,
   saved BOOLEAN DEFAULT FALSE
+  FOREIGN KEY (usersId) REFERENCES users (id) ON DELETE CASCADE
   );
 
 CREATE TABLE users (
   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   username VARCHAR(50) NOT NULL,
   firstName VARCHAR(100) NOT NULL,
-  lastName VARCHAR(100),
-  FOREIGN KEY (listingsId) REFERENCES listings (id) ON DELETE CASCADE
+  lastName VARCHAR(100)
 );
 
+CREATE TABLE relationUsersListings (
+  userId INT NOT NULL,
+  listingsId INT NOT NULL,
+  FOREIGN KEY (userId) REFERENCES users (id) ON DELETE CASCADE,
+  FOREIGN KEY (listingsId) REFERENCES listings (id) ON DELETE CASCADE,
+  UNIQUE (usersID, listingsId)
+);

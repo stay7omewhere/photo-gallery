a82f3f4af211d19d564a6cb0b939ca134fddfc9d
Faith <pfaithmtan@gmail.com>
2019-11-04 12:32:02 -0800

    Generate users using Node drain event

---
+++ database/generateUsers.js
@@ -0,0 +1,38 @@
+const faker = require('faker');
+const fs = require('fs');
+
+const writeUsers = fs.createWriteStream('users.csv');
+writeUsers.write('id,username,firstName,lastName\n', 'utf8');
+
+function writeTenMillionUsers(writer, encoding, callback) {
+  let i = 10000000;
+  let id = 0;
+  function write() {
+    let ok = true;
+    do {
+      i -= 1;
+      id += 1;
+      const username = faker.internet.userName();
+      const firstName = faker.name.firstName();
+      const lastName = faker.name.lastName();
+      const data = `${id},${username},${firstName},${lastName}\n`;
+      if (i === 0) {
+        writer.write(data, encoding, callback);
+      } else {
+        // see if we should continue, or wait
+        // don't pass the callback, because we're not done yet.
+        ok = writer.write(data, encoding);
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+      // had to stop early!
+      // write some more once it drains
+      writer.once('drain', write);
+    }
+  }
+  write()
+}
+
+writeTenMillionUsers(writeUsers, 'utf-8', () => {
+  writeUsers.end();
+});

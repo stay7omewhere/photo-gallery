77056f2f78cbcb86473f13d70a91c62d5d342c84
Faith <pfaithmtan@gmail.com>
2019-11-04 22:09:44 -0800

    Generate photos
    - Include an average of 15 photos per listing

---
+++ generatePhotos.js
@@ -0,0 +1,33 @@
+const faker = require('faker');
+const fs = require('fs');
+
+const writeUsers = fs.createWriteStream('photos.csv');
+writeUsers.write('id,photoUrl,description\n', 'utf8');
+
+function writeOneHundredAndFiftyMillionPhotos(writer, encoding, callback) {
+  let i = 150000000;
+  let id = 0;
+  function write() {
+    let ok = true;
+    do {
+      i -= 1;
+      id += 1;
+      const photoUrl = faker.image.image();
+      const description = faker.lorem.sentence();
+      const data = `${id},${photoUrl},${description}\n`;
+      if (i === 0) {
+        writer.write(data, encoding, callback);
+      } else {
+        ok = writer.write(data, encoding);
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+      writer.once('drain', write);
+    }
+  }
+  write()
+}
+
+writeOneHundredAndFiftyMillionPhotos(writeUsers, 'utf-8', () => {
+  writeUsers.end();
+});

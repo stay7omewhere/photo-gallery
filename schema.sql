e131c0ca4363744fcf4c73015fcdec07177d62bd
Faith <pfaithmtan@gmail.com>
2019-11-06 21:49:27 -0800

    Import users CSV file to users table

--- schema.sql
+++ schema.sql
@@ -4,18 +4,18 @@ CREATE DATABASE photo_gallery;
 USE photo_gallery;
 
 CREATE TABLE photos (
-  id INT SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   listingId INT NOT NULL,
   photoUrl TEXT NOT NULL,
-  photoDescription VARCHAR(160),
+  photoDescription VARCHAR(160)
 );
 
 CREATE TABLE listings (
-  id INT SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY
 );
 
 CREATE TABLE users (
-  id INT SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   username VARCHAR(50) NOT NULL,
   firstName VARCHAR(100) NOT NULL,
   lastName VARCHAR(100)
@@ -24,9 +24,12 @@ CREATE TABLE users (
 CREATE TABLE relationUsersListings (
   userId INT NOT NULL,
   listingId INT NOT NULL,
-  UNIQUE (usersID, listingsId)
+  UNIQUE (userID, listingId)
 );
 
-ALTER TABLE photos ADD FOREIGN KEY (listingsId) REFERENCES listings (id) ON DELETE CASCADE;
-ALTER TABLE relationUsersListings ADD FOREIGN KEY (usersId) REFERENCES users (id) ON DELETE CASCADE;
-ALTER TABLE relationUsersListings ADD FOREIGN KEY (listingsId) REFERENCES listings (id) ON DELETE CASCADE;
+ALTER TABLE photos ADD FOREIGN KEY (listingId) REFERENCES listings (id) ON DELETE CASCADE;
+ALTER TABLE relationUsersListings ADD FOREIGN KEY (userId) REFERENCES users (id) ON DELETE CASCADE;
+ALTER TABLE relationUsersListings ADD FOREIGN KEY (listingId) REFERENCES listings (id) ON DELETE CASCADE;
+
+COPY users(username,firstName,lastName)
+FROM '/Users/Faith/Documents/SDC/photo-gallery/database/csvdatafiles/users.csv' DELIMITER ',' CSV HEADER;

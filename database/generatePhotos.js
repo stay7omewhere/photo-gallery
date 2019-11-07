2b00e25496d46ca81d0da5f2b50d9587e3f581b5
Faith <pfaithmtan@gmail.com>
2019-11-06 22:41:09 -0800

    Add listingID
    - Increment listing ID for every 12 photos

--- database/generatePhotos.js
+++ database/generatePhotos.js
@@ -3,11 +3,12 @@ const fs = require('fs');
 const path = require('path');
 
 const writeUsers = fs.createWriteStream(path.resolve(__dirname, './csvdatafiles/photos.csv'));
-writeUsers.write('id,photoUrl,description\n', 'utf8');
+writeUsers.write('listingId,photoUrl,description\n', 'utf8');
 
 writeOneHundredAndTwentyMillionPhotos = (writer, encoding, callback) => {
   let i = 120000000;
   let id = 0;
+  let j = 1;
   write = () => {
     let ok = true;
     do {
@@ -16,9 +17,15 @@ writeOneHundredAndTwentyMillionPhotos = (writer, encoding, callback) => {
       if (id % 100000 === 0) {
         console.log(id);
       }
+
+      if (id % 12 === 0) {
+        j += 1;
+      }
+
+      const listingId = j;
       const photoUrl = faker.image.image();
       const description = faker.lorem.sentence();
-      const data = `${id},${photoUrl},${description}\n`;
+      const data = `${listingId},${photoUrl},${description}\n`;
       if (i === 0) {
         writer.write(data, encoding, callback);
       } else {

cc161bf0390f7eb93c7374086142f4ab40878a9f
Faith <pfaithmtan@gmail.com>
2019-11-08 15:33:18 -0800

    Generate CSV for photos table in Cassandra

---
+++ database/generatePhotosWithId.js
@@ -0,0 +1,42 @@
+const faker = require('faker');
+const fs = require('fs');
+const path = require('path');
+
+const writeUsers = fs.createWriteStream(path.resolve(__dirname, './csvdatafiles/photoswithid.csv'));
+writeUsers.write('id,listingId,photoUrl,photoDescription\n', 'utf8');
+
+writeOneHundredAndTwentyMillionPhotosWithId = (writer, encoding, callback) => {
+  let i = 120000000;
+  let id = 0;
+  let j = 1;
+  write = () => {
+    let ok = true;
+    do {
+      i -= 1;
+      id += 1;
+      if (id % 100000 === 0) {
+        console.log(id);
+      }
+      const listingId = j;
+      if (id % 12 === 0) {
+        j += 1;
+      }
+      const photoUrl = faker.image.image();
+      const photoDescription = faker.lorem.sentence();
+      const data = `${id},${listingId},${photoUrl},${photoDescription}\n`;
+      if (i === 0) {
+        writer.write(data, encoding, callback);
+      } else {
+        ok = writer.write(data, encoding);
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+      writer.once('drain', write);
+    }
+  };
+  write();
+};
+
+writeOneHundredAndTwentyMillionPhotosWithId(writeUsers, 'utf-8', () => {
+  writeUsers.end();
+});

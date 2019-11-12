4c32b760bf77884b47f727231c59e5967a42429e
Faith <pfaithmtan@gmail.com>
2019-11-12 15:37:30 -0800

    Implement logic for creating listingIds
    - Ensure no user has saved same listing twice

--- database/generateRelationsUsersListings.js
+++ database/generateRelationsUsersListings.js
@@ -5,10 +5,9 @@ const path = require('path');
 const writeUsers = fs.createWriteStream(path.resolve(__dirname, './csvdatafiles/relationUsersListings.csv'));
 writeUsers.write('userId,listingId\n', 'utf8');
 
-writeSeventyMillionUserSavedListings = (writer, encoding, callback) => {
-  let i = 70000000;
+writeTwentyMillionUserSavedListings = (writer, encoding, callback) => {
+  let i = 200000000;
   let id = 0;
-  let j = 1;
   write = () => {
     let ok = true;
     do {
@@ -17,17 +16,34 @@ writeSeventyMillionUserSavedListings = (writer, encoding, callback) => {
       if (id % 100000 === 0) {
         console.log(id);
       }
-      const userId = j;
-      if (id % 7 === 0) {
-        j += 1;
+
+      const userId = id;
+
+      let listings = new Set([]);
+
+      for (let j = 1; j <= 20; j++) {
+        let listingId = Math.floor(Math.random() * Math.floor(10000000)) + 1;
+
+        const IsUniqueThenAddUniqueListing = (listingId) => {
+          if (!listings.has(listingId)) {
+          listings.add(listingId);
+          } else {
+            const randomInt = Math.floor(Math.random() * Math.floor(10000000)) + 1;
+            IsUniqueThenAddUniqueListing(randomInt);
+          }
+        }
+
+        IsUniqueThenAddUniqueListing(listingId)
+
+        const data = `${userId},${listingId}\n`;
+
+        if (i === 0) {
+          writer.write(data, encoding, callback);
+        } else {
+          ok = writer.write(data, encoding);
+        }
       }
-      const listingId = Math.floor(Math.random() * Math.floor(10000000)) + 1;
-      const data = `${userId},${listingId}\n`;
-      if (i === 0) {
-        writer.write(data, encoding, callback);
-      } else {
-        ok = writer.write(data, encoding);
-      }
+
     } while (i > 0 && ok);
     if (i > 0) {
       writer.once('drain', write);
@@ -36,6 +52,6 @@ writeSeventyMillionUserSavedListings = (writer, encoding, callback) => {
   write();
 };
 
-writeSeventyMillionUserSavedListings(writeUsers, 'utf-8', () => {
+writeTwentyMillionUserSavedListings(writeUsers, 'utf-8', () => {
   writeUsers.end();
 });

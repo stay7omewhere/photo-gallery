3c4ca39292fb77310c165ff6f1db2e703d242f04
Faith <pfaithmtan@gmail.com>
2019-11-13 10:50:11 -0800

    Handle GET request
    - Import database methods as controllers
    - Remove other HTTP requests

--- server/index.js
+++ server/index.js
@@ -1,30 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3001;
-const db = require('../database/index.js');
+const controllers = require('../database/dbMethods.js');
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
-app.use('/:listingid', express.static('public'));
 
-app.get('/:listingid', (req, res) => {
-  res.end();
-});
+app.use('/rooms/:id', express.static('public'));
 
-app.get('/photos/:listingid', (req, res) => {
-  db.findListing(req.params.listingid)
-    .then((data) => {
-      var listing = data[0];
-      console.log('Photos retrieved: ', listing.photos);
-      res.send(listing.photos);
-    })
-    .catch((err) => {
-      console.log('Error retrieving photos: ', err);
-      res.end();
-    });
-});
+app.get('/api/rooms/:id/photos/', controllers.getPhotosOfListing);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));

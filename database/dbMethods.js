03f911d0d8ef2b5e32276f9c192c79854aff96b1
Faith <pfaithmtan@gmail.com>
2019-11-13 19:36:31 -0800

    Save listing to database for specific user

--- database/dbMethods.js
+++ database/dbMethods.js
@@ -5,11 +5,25 @@ module.exports = {
     const query = `SELECT * FROM photos WHERE listingId = ${req.params.id}`;
     db.client.execute(query, { prepare: true })
       .then(result => {
-        console.log('success!');
+        console.log('getting the photos!');
         res.status(200).send(result.rows);
       })
       .catch(error => {
-        console.log('error!!!');
+        console.log('error GETting');
+        res.status(500).send(error);
+      })
+  },
+
+  saveListing: (req, res) => {
+    const query = `INSERT INTO users (id, savedListingId, username, firstName, lastName) VALUES (${req.params.id}, ${req.params.savedListingId}, "${req.params.username}", "${req.params.firstName}", "${req.params.lastName}")`;
+
+    db.client.execute(query, { prepare: true })
+      .then(result => {
+        console.log('posted to db!');
+        res.status(200).send(result);
+      })
+      .catch(error => {
+        console.log('error with POSTing');
         res.status(500).send(error);
       })
   }

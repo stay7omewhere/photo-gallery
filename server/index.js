de9f8f32dfa2e20bb9ceb10608eea72d41bf265d
Faith <pfaithmtan@gmail.com>
2019-11-13 19:37:17 -0800

    Listen to post request
    - Handle saved listings for user

--- server/index.js
+++ server/index.js
@@ -13,5 +13,6 @@ app.use(express.json());
 app.use('/rooms/:id', express.static('/Users/Faith/Documents/photo-gallery/public'));
 
 app.get('/api/rooms/:id/photos/', controllers.getPhotosOfListing);
+app.post('/api/rooms/:id/save', controllers.saveListing);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
